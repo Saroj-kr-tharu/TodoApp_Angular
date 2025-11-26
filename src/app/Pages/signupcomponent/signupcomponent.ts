3bfc12b8386cfd31fcaffa069c4da1a81927090a
Saroj-kr-tharu <sarojkumartharu11234@gmail.com>
2025-11-26 17:34:10 +0545

    done custum validation reapet password

--- src/app/Pages/signupcomponent/signupcomponent.ts
+++ src/app/Pages/signupcomponent/signupcomponent.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators, } from '@angular/forms';
 
 import { FormFieldConfig, ValidationConfig } from '../../Model/FormSignin.type';
+
+
 
 @Component({
   selector: 'app-signupcomponent',
@@ -10,24 +12,69 @@ import { FormFieldConfig, ValidationConfig } from '../../Model/FormSignin.type';
   styleUrl: './signupcomponent.css',
 })
 export class Signupcomponent {
+
+    passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+        const password = control.get('password');
+        const repeatPassword = control.get('repeatpassword');
+        
+        if (!password || !repeatPassword) {
+          console.log(`password => ${password} and repeat => ${repeatPassword} `)
+            return null;
+        }
+        
+        return password.value === repeatPassword.value ? null : { passwordMismatch: true };
+    };
+
+    // Custom validator for repeat password field
+    repeatPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+        if (!this.signupForm) return null;
+        
+        const password = this.signupForm.get('password');
+        if (!password) return null;
+        
+        return password.value === control.value ? null : { passwordMismatch: true };
+    };
     
       signupForm: FormGroup = new FormGroup({
-        userId: new FormControl(0),
-        emailId: new FormControl('', [Validators.required, Validators.minLength(5), Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6), ]), 
-        fullName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        mobileNo: new FormControl('', [Validators.required, Validators.pattern('^\\d{10}$')])
-
-      });
+        email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]), 
+        repeatpassword: new FormControl('', [
+            Validators.required, 
+            Validators.minLength(6),
+            this.repeatPasswordValidator
+        ]),
+        username: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      }, { validators: this.passwordMatchValidator });
 
         
    signupFormConfig : FormFieldConfig[] = [
-    { type: 'number', id: 'userId', label: 'User ID', placeholder:'id', autocomplete: 'username', validation: {} },
-    { type: 'email', id: 'emailId', label: 'Email Address', placeholder:'email ... ', autocomplete: 'email', validation: { 'required': 'Email is required', 'email': 'Please enter a valid email address' } },
-    { type: 'password', id: 'password', label: 'Password', placeholder:'password .. ', autocomplete: 'current-password', validation: { 'required': 'Password is required', 'minlength': 'Password must be at least 6 characters long' } },
-    { type: 'text', id: 'fullName', label: 'Full Name', placeholder:'fullname ..', autocomplete: 'name', validation: {'required': 'username is required', 'minlength':'minlength 2 char '} },
-    { type: 'tel', id: 'mobileNo', label: 'Mobile Number', placeholder:'mobile no ..', autocomplete: 'tel', validation: {'required': 'mobile is required', 'pattern':'mobile no is not valid '} }
+    { type: 'email', id: 'email', label: 'Email Address', placeholder:'Enter email ... ', autocomplete: 'email', validation: { 'required': 'Email is required', 'email': 'Please enter a valid email address' } },
+    { type: 'text', id: 'username', label: 'Full Name', placeholder:'Enter username ..', autocomplete: 'name', validation: {'required': 'username is required', 'minlength':'minlength 2 char '} },
+    { type: 'password', id: 'password', label: 'Password', placeholder:'Enter password .. ', autocomplete: 'current-password', validation: { 'required': 'Password is required', 'minlength': 'Password must be at least 6 characters long' } },
+
+
+       { type: 'password', id: 'repeatpassword', label: 'Repeat Password', placeholder:'Repeat password .. ', autocomplete: 'current-password', validation: { 'required': 'Password is required', 'minlength': 'Password must be at least 6 characters long', 'passwordMismatch': 'Passwords do not match' } },
+    
   ];
+
+
+  
+
+
+get passwordMismatch() {
+  return (this.signupForm.hasError('passwordMismatch') || this.signupForm.get('repeatpassword')?.hasError('passwordMismatch')) && 
+         (this.signupForm.get('password')?.touched || this.signupForm.get('repeatpassword')?.touched);
+}
+
+
+     markFormGroupTouched() {
+        Object.keys(this.signupForm.controls).forEach(field => {
+            const control = this.signupForm.get(field);
+            control?.markAsTouched({ onlySelf: true });
+        });
+    }
+
+
 
   getValidationKeys(validation: ValidationConfig): string[] {
     return Object.keys(validation);
